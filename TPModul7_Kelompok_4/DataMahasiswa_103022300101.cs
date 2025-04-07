d6d63294b7815ca13599446c6f5f0ecd00eeaaf0
Kuroakai <derudj12345@gmail.com>
2025-04-07 20:45:34 +0700

    menambahkan class DataMahasiswa

---
+++ TPModul7_Kelompok_4/DataMahasiswa_103022300101.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class DataMahasiswa103022300088
+{
+    public class Nama
+    {
+        [JsonPropertyName("depan")]
+        public string Depan { get; set; }
+
+        [JsonPropertyName("belakang")]
+        public string Belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        [JsonPropertyName("nama")]
+        public Nama Nama { get; set; }
+
+        [JsonPropertyName("nim")]
+        public string Nim { get; set; }
+
+        [JsonPropertyName("fakultas")]
+        public string Fakultas { get; set; }
+    }
+
+    public static void ReadJSON(string filePath)
+    {
+        try
+        {
+            string jsonString = File.ReadAllText(filePath);
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            Console.WriteLine($"Nama {mhs.Nama.Depan} {mhs.Nama.Belakang} dengan nim {mhs.Nim} dari fakultas {mhs.Fakultas}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Terjadi kesalahan saat deserialisasi: " + ex.Message);
+        }
+    }
+}
--- TPModul7_Kelompok_4/Program.cs
+++ TPModul7_Kelompok_4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa103022300088.ReadJSON(@"D:\telkom\coding\C#\tpmodul7\TPModul7_Kelompok_4\tp7_1_103022300101.json");
+        Console.WriteLine(" ");
+        //KuliahMahasiswa103022300088.ReadJson(@"C:\Users\rakad\Downloads\tpmod7_103022300088\TPModul7_Kelompok_4\TPModul7_Kelompok_4\tp7_2_103022300088.json");
+    }
+}
