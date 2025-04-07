78faaf89b9039690a254a91dda4dc465e9526749
sisadul <abdulazizsaepurohmat1@gmail.com>
2025-04-07 21:10:19 +0700

    menambahkan class abdul_KuliahMahasiswa_103022300092

--- TPModul7_Kelompok_4/Program.cs
+++ TPModul7_Kelompok_4/Program.cs
@@ -6,6 +6,8 @@ class Program
     static void Main(string[] args)
     {
         DataMahasiswa_103022300092_Abdul.ReadJSON(@"C:\Users\ABD AZIZ\Source\Repos\TPModul7_Kelompok_4\TPModul7_Kelompok_4\tp7_1_103022300092.json");
+        Console.WriteLine("");
+        KuliahMahasiswa_103022300092_Abdul.ReadJson(@"C:\Users\ABD AZIZ\Source\Repos\TPModul7_Kelompok_4\TPModul7_Kelompok_4\tp7_2_103022300092.json");
 
     }
 }
