ad40f4f1814e94a446e61025aa40950e16a16c6f
Kuroakai <derudj12345@gmail.com>
2025-04-07 21:05:47 +0700

    Menambah penjelasan

--- TPModul7_Kelompok_4/KuliahMahasiswa_103022300101.cs
+++ TPModul7_Kelompok_4/KuliahMahasiswa_103022300101.cs
@@ -1,36 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; // Buat pake List<T>
 using System.Linq;
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Text.Json; // Buat deserialisasi JSON
+using System.Text.Json.Serialization; // Buat mapping nama properti JSON
 using System.Threading.Tasks;
 
 namespace TPModul7_Kelompok_4
 {
     class KuliahMahasiswa_103022300101
     {
+        // Kelas untuk struktur data course di JSON
         public class Course
         {
-            [JsonPropertyName("code")]
+            [JsonPropertyName("code")] // Mapping ke JSON "code"
             public String Code { get; set; }
 
-            [JsonPropertyName("name")]
+            [JsonPropertyName("name")] // Mapping ke JSON "name"
             public String Name { get; set; }
         }
 
+        // Kelas pembungkus list course dari JSON
         public class List
         {
-            [JsonPropertyName("courses")]
+            [JsonPropertyName("courses")] // Mapping ke JSON "courses" yang isinya array objek Course
             public List<Course> Courses { get; set; }
         }
 
+        // Method buat baca JSON dan deserialisasi ke objek List
         public static void ReadJson(string filepath)
         {
             try
             {
-                String jsonString = File.ReadAllText(filepath);
-                List list = JsonSerializer.Deserialize<List>(jsonString);
+                String jsonString = File.ReadAllText(filepath); // Baca isi file JSON
+                List list = JsonSerializer.Deserialize<List>(jsonString); // Ubah JSON jadi objek List
+
+                // Tampilkan semua data course yang ada
                 foreach (var course in list.Courses)
                 {
                     Console.WriteLine($"Code: {course.Code}, Name: {course.Name}");
@@ -38,6 +43,7 @@ namespace TPModul7_Kelompok_4
             }
             catch (Exception ex)
             {
+                // Handle error saat proses deserialisasi
                 Console.WriteLine("Kegagalan Deserialisasi JSON");
             }
         }
--- TPModul7_Kelompok_4/Program.cs
+++ TPModul7_Kelompok_4/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using TPModul7_Kelompok_4;
+using TPModul7_Kelompok_4; // Import namespace yang isinya class DataMahasiswa dan KuliahMahasiswa
 
 class program
 {
     static void Main(string[] args)
     {
+        // Panggil method ReadJSON dari class DataMahasiswa buat baca file JSON mahasiswa
         DataMahasiswa103022300101.ReadJSON(@"D:\telkom\coding\C#\tpmodul7\TPModul7_Kelompok_4\tp7_1_103022300101.json");
-        Console.WriteLine(" ");
+
+        Console.WriteLine(" "); // Biar ada jarak/pemisah di output console
+
+        // Panggil method ReadJson dari class KuliahMahasiswa buat baca file JSON course mata kuliah
         KuliahMahasiswa_103022300101.ReadJson(@"D:\telkom\coding\C#\tpmodul7\TPModul7_Kelompok_4\tp7_2_103022300101.json");
     }
 }
