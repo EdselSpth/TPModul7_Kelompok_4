ef9d5c4f772c6425c0b28f9217c39d4afcc6d963
Funny-pixel765 <funnyrnd92@gmail.com>
2025-04-07 21:21:08 +0700

    menambahkan class <Funny_KuliahMahasiswa103022330036_TioFunnyTinambunan>”

--- TPModul7_Kelompok_4/Program.cs
+++ TPModul7_Kelompok_4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TPModul7_Kelompok_4;
 
 class program
 {
@@ -6,5 +7,6 @@ class program
     {
         DataMahasiswa103022330036_Funny.ReadJSON(@"C:\Users\Haikal Cannavaro\source\repos\TPModul7_Kelompok_4\TPModul7_Kelompok_4\tp7_1_103022330036.json");
         Console.WriteLine(" ");
+        KuliahMahasiswa103022330036_TioFunnyTinambunan.ReadJson(@"C:\Users\Haikal Cannavaro\source\repos\TPModul7_Kelompok_4\TPModul7_Kelompok_4\tp7_2_103022330036.json");
     }
 }
